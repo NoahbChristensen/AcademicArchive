c9f9b68f00c6a3276270cf85b77f604aa479fe0f
NoahbChristensen <146462431+NoahbChristensen@users.noreply.github.com>
2024-02-29 11:50:24 -0600

    Changed variable names

--- AcademicExamples/PrimesLogPlot.py
+++ AcademicExamples/PrimesLogPlot.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-def is_prime(n):
-    if n == 2:
+def is_prime(primeNumberCandidate):
+    if primeNumberCandidate == 2:
         return True
-    if n == 1 or n % 2 == 0:
+    if primeNumberCandidate == 1 or primeNumberCandidate % 2 == 0:
         return False
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        if n % i == 0:
+    for i in range(3, int(math.sqrt(primeNumberCandidate)) + 1, 2):
+        if primeNumberCandidate % i == 0:
             return False
     return True
 
@@ -19,12 +19,12 @@ def count_primes(n):
             count += 1
     return count
 
-n = [10, 100, 1000, 10000, 100000, 1000000]
-primes = [count_primes(i) for i in n]
+numberRange = [10, 100, 1000, 10000, 100000, 1000000]
+primes = [count_primes(i) for i in numberRange]
 
-plt.plot(n, primes, 'o-')
+plt.plot(numberRange, primes, 'o-')
 plt.yscale('log')
-plt.xlabel('n')
+plt.xlabel('Range of Numbers')
 plt.ylabel('Number of primes less than n')
 plt.title('Number of primes less than n')
 plt.show()
